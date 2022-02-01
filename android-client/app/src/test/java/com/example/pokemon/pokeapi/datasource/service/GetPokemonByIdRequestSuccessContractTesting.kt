c503d030b6ff52b972d41b4e63d67e17740ea4fb
danielUrru <durrutxua@baturamobile.com>
2022-02-01 13:29:57 +0100

    test: wiremock get pokemon by id

--- android-client/app/src/test/java/com/example/pokemon/pokeapi/datasource/service/GetPokemonByIdRequestSuccessContractTesting.kt
+++ android-client/app/src/test/java/com/example/pokemon/pokeapi/datasource/service/GetPokemonByIdRequestSuccessContractTesting.kt
@@ -1,8 +1,6 @@
 package com.example.pokemon.pokeapi.datasource.service
 
-import com.example.pokemon.pokeapi.PokeApiService
 import com.example.pokemon.pokeapi.serviceLocator
-import com.github.tomakehurst.wiremock.client.WireMock.*
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import com.github.tomakehurst.wiremock.junit.WireMockRule
 import org.hamcrest.MatcherAssert.assertThat
@@ -11,32 +9,32 @@ import org.hamcrest.Matchers.notNullValue
 import org.hamcrest.core.IsEqual
 import org.junit.Rule
 import org.junit.Test
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import kotlin.reflect.typeOf
 
-class GetAllPokemonRequestSuccessContractTest {
+class GetPokemonByIdRequestSuccessContractTesting {
 
     @get:Rule
     val wireMockRule = WireMockRule(WireMockConfiguration.wireMockConfig().dynamicPort())
 
     @Test
-    fun successfulRequestForAllPokemon() {
+    fun successfulRequestForASinglePokemon() {
 
         val service = serviceLocator(wireMockRule.baseUrl())
 
-        val result = service.getPokemonList().execute()
+        val result = service.getPokemonById("0").execute()
 
         assertThat(result.code(), IsEqual(200))
         assertThat(result.body(), notNullValue())
-        assertThat(result.body(), hasSize(1))
 
-        val pokemon = result.body()!![0]
+        val pokemon = result.body()!!
 
         assertThat(pokemon.name, notNullValue())
         assertThat(pokemon.id, notNullValue())
         assertThat(pokemon.types, hasSize(2))
         assertThat(pokemon.favourite, notNullValue())
+        assertThat(pokemon.stats, notNullValue())
+        assertThat(pokemon.abilities, hasSize(2))
+        assertThat(pokemon.height, notNullValue())
+        assertThat(pokemon.weight, notNullValue())
 
     }
 }
